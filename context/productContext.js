3a3de399a94a99d70cc3ade9f0a34c6514668528
Imogen <90554214+minoi001@users.noreply.github.com>
2023-03-13 11:52:48 +0000

    update product context

--- context/productContext.js
+++ context/productContext.js
@@ -4,70 +4,54 @@ import { createCheckout, updateCheckout } from "../lib/shopify";
 const ProductContext = createContext();
 
 export default function ProductProvider({ children }) {
-  const [cart, setCart] = useState([]);
-  const [cartOpen, setCartOpen] = useState(false);
-  const [checkoutId, setCheckoutId] = useState("");
-  const [checkoutUrl, setCheckoutUrl] = useState("");
+  const [productInfo, setProductInfo] = useState({
+    barcode: "",
+    barcodeURL: "",
+    variantID: "",
+    photography: "",
+    styleName: "",
+    photosCount: 0,
+    cdnLink: "",
+    imageVariables: [
+      {
+        src: "",
+      },
+    ],
+    additionalDetails: "",
+    type: "",
+    RRP: "",
+    price: "",
+    condition: "",
+    faults: "",
+    packaging: "",
+    age: "",
+    influencer: "",
+    anonymous: "No",
+    gender: "",
+    brand: "",
+    colour: "",
+    size: "",
+    occasion: [],
+    sleeveLength: "",
+    dressType: "",
+    trouserFit: "",
+    bottomsRise: "",
+    bagType: [],
+    tags: [],
+    description: "",
+    SKU: "",
+  });
 
-  useEffect(() => {
-    if (localStorage.checkout_id) {
-      const cartObject = JSON.parse(localStorage.checkout_id);
-      if (cartObject[0].id) {
-        setCart([cartObject[0]]);
-      } else if (cartObject[0].length > 0) {
-        setCart(...[cartObject[0]]);
-      }
-
-      setCheckoutId(cartObject[1].id);
-      setCheckoutUrl(cartObject[1].webUrl);
-    }
-  }, []);
-
-  async function addToCart(newItem) {
-    if (cart.length === 0) {
-      setCart([newItem]);
-      const checkout = await createCheckout(
-        newItem.variantId,
-        newItem.variantQuantity
-      );
-
-      setCheckoutId(checkout.id);
-      setCheckoutUrl(checkout.webUrl);
-
-      localStorage.setItem("checkout_id", JSON.stringify([newItem, checkout]));
-    } else {
-      let newCart = [];
-      let added = false;
-
-      cart.map((item) => {
-        if (item.id === newItem.id) {
-          item.variantQuantity++;
-          newCart = [...cart];
-          added = true;
-        }
-      });
-
-      if (!added) {
-        newCart = [...cart, newItem];
-      }
-
-      setCart(newCart);
-      const newCheckout = await updateCheckout(checkoutId, newCart);
-      localStorage.setItem(
-        "checkout_id",
-        JSON.stringify([newCart, newCheckout])
-      );
-    }
+  function updateProductValue(value, label) {
+    setProductInfo({ ...productInfo, [productInfo[label]]: value });
   }
 
   return (
     <ProductContext.Provider
       value={{
-        cart,
-        cartOpen,
-        setCartOpen,
-        addToCart,
-        checkoutUrl,
+        productInfo,
+        setProductInfo,
+        updateProductValue,
       }}
     >
       {children}
