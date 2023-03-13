70627ca807f2327239d38a53716f8d4b05768012
Imogen <90554214+minoi001@users.noreply.github.com>
2023-03-13 20:08:38 +0000

    working on user login

--- context/accountContext.js
+++ context/accountContext.js
@@ -1,23 +1,84 @@
 import { createContext, useState, useEffect } from "react";
-import { getUserInfo } from "../lib/shopify";
+import { getUserAccessToken, getUserInfo } from "../lib/shopify";
 
 const AccountContext = createContext();
 
 export default function AccountProvider({ children }) {
-  function sendUserRequest() {
-    let email = "office@reliked.com";
-    let password = "acp1rvz!TUP7uka4ehn";
-    getUserInfo(email, password);
-  }
   const [userInfo, setUserInfo] = useState({
     userType: "",
     userName: "",
     listerCode: "",
-    email: "",
-    password: "",
+    email: "office@reliked.com",
+    password: "acp1rvz!TUP7uka4ehn",
     errorMessage: "",
-    loginStatus: "",
+    loginStatus: false,
+    token: "",
   });
+
+  function updateUserValue(
+    value,
+    label,
+    value1,
+    label1,
+    value2,
+    label2,
+    value3,
+    label3
+  ) {
+    userInfo[label] = value;
+    userInfo[label1] = value1;
+    userInfo[label2] = value2;
+    userInfo[label3] = value3;
+    return value;
+  }
+
+  async function sendUserRequest() {
+    // send API req for token
+    const tokenRequest = await getUserAccessToken(
+      userInfo.email,
+      userInfo.password
+    );
+    // if credentials fail to produce token, set error message and login status to false
+    if (!tokenRequest.customerAccessTokenCreate.customerAccessToken) {
+      updateUserValue(
+        "we couldn't log you in with those details",
+        "errorMessage",
+        false,
+        "loginStatus"
+      );
+      // if token, update user info with token
+    } else {
+      updateUserValue(
+        `${tokenRequest.customerAccessTokenCreate.customerAccessToken.accessToken}`,
+        "token"
+      );
+      // request user info using token
+      const infoRequest = await getUserInfo(
+        tokenRequest.customerAccessTokenCreate.customerAccessToken.accessToken
+      );
+      // if token value, set user info values
+      if (infoRequest.customer) {
+        updateUserValue(
+          `${infoRequest.customer.firstName} ${infoRequest.customer.lastName}`,
+          "userName",
+          `${infoRequest.customer.userType.value}`,
+          "userType",
+          `${infoRequest.customer.userCode.value}`,
+          "listerCode",
+          true,
+          "loginStatus"
+        );
+        // token invalid, set error message and login status to false
+      } else {
+        updateUserValue(
+          "your session has expired, please login again",
+          "errorMessage",
+          false,
+          "loginStatus"
+        );
+      }
+    }
+  }
 
   useEffect(() => {
     sendUserRequest();
