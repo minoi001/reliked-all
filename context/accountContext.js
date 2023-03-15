5c2b42785d90fd35acd6af8b831a994e500cb68d
Imogen Minoli <imogenminoli@Imogens-MacBook-Air-5.local>
2023-03-15 10:40:53 +0000

    errors since moving to m1 mac

--- context/accountContext.js
+++ context/accountContext.js
@@ -80,9 +80,9 @@ export default function AccountProvider({ children }) {
     }
   }
 
-  useEffect(() => {
-    sendUserRequest();
-  }, []);
+  // useEffect(() => {
+  //   sendUserRequest();
+  // }, []);
 
   return (
     <AccountContext.Provider
