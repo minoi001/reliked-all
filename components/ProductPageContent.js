decbd12c70a88447fdffb547d4827afb99885dd2
Imogen Minoli <imogen.minoli@gmail.com>
2023-03-20 22:09:26 +0000

    product page image formatting

--- components/ProductPageContent.js
+++ components/ProductPageContent.js
@@ -6,7 +6,7 @@ export default function ProductPageContent({ product }) {
       <div className="flex flex-col justify-center items-center space-y-8 md:flex-row md:items-start md:space-y-0 md:spacex-4 lg:space-x-8 max-w-6xl w-11/12 mx-auto z-0">
         {/* {product.title} */}
         <div className="w-full max-w-md border bg-white overflow-hidden shadow-lg md:w-1/2">
-          <div className="z--100 h-full w-full p-5">
+          <div className="grid py-14 place-items-center">
             <Image
               src={
                 product.images
@@ -16,7 +16,7 @@ export default function ProductPageContent({ product }) {
               alt={product.title}
               height="2000"
               width="1000"
-              className="z-0"
+              className="z-0 object-contain center max-h-96"
             />
           </div>
         </div>
