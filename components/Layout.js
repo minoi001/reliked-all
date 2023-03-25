c331d2e63ee1c21862ce42375f355c2ce37b7972
Imogen <90554214+minoi001@users.noreply.github.com>
2023-03-25 12:26:21 +0000

    conditional subheaders

--- components/Layout.js
+++ components/Layout.js
@@ -1,12 +1,21 @@
+import { useRouter } from "next/router";
 import Footer from "./Footer";
 import MegaMenu from "./MegaMenu";
 
 export default function Layout({ children }) {
+  const { pathname } = useRouter();
+
   return (
     <div>
       <header>
         <MegaMenu />
       </header>
+      {pathname?.includes("/selling") ? (
+        <subheader>selling subheader</subheader>
+      ) : (
+        <subheader>shopping subheader</subheader>
+      )}
+
       <main>{children}</main>
       <footer>
         <Footer />
