25d821c71d2cbd3885d7c916e40f20812daea43c
Imogen <90554214+minoi001@users.noreply.github.com>
2023-03-25 12:07:00 +0000

    various minor changes

---
+++ components/Selling/ListingVariables/Staff.js
@@ -0,0 +1,18 @@
+import React, { useContext } from "react";
+import { ProductContext } from "../../../context/productContext";
+import { AccountContext } from "../../../context/accountContext";
+const Staff = ({ products }) => {
+  const { productInfo } = useContext(ProductContext);
+  const { userInfo } = useContext(AccountContext);
+
+  return (
+    <div>
+      <fieldset className="px-4 border-solid border-2 border-indigo-600 ">
+        <legend className="p-1">STAFF</legend>
+        <input className="mb-4 p-2" placeholder={"Tags"} />
+      </fieldset>
+    </div>
+  );
+};
+
+export default Staff;
---
+++ pages/selling.js
@@ -0,0 +1,48 @@
+import NewListing from "../components/Selling/NewListing";
+import { getAllProducts, getProduct } from "../lib/shopify.js";
+
+import { AccountContext } from "../context/accountContext";
+import { useContext } from "react";
+import Login from "../components/Login";
+import Link from "next/link";
+
+export default function Selling({ product }) {
+  const { userInfo } = useContext(AccountContext);
+  return (
+    <div>
+      {userInfo.loginStatus ? (
+        <div>
+          <div className="minh-screen py-12 sm:pt-20">selling page</div>
+          <Link href="/selling/add-listing">Add listing</Link>
+        </div>
+      ) : (
+        <Login />
+      )}
+    </div>
+  );
+}
+
+// export async function getStaticPaths() {
+//   const products = await getAllProducts();
+//   const paths = products.map((item) => {
+//     const handle = String(item.node.handle);
+
+//     return {
+//       params: { product: handle },
+//     };
+//   });
+//   return {
+//     paths,
+//     fallback: false,
+//   };
+// }
+
+// export async function getStaticProps({ params }) {
+//   const product = await getProduct(params.product);
+
+//   return {
+//     props: {
+//       product,
+//     },
+//   };
+// }
