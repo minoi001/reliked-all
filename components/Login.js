d67224768b8a7c2371ff393dc21a10376f581bb9
Imogen <90554214+minoi001@users.noreply.github.com>
2023-03-25 11:33:11 +0000

    slight changes to persistent login

--- components/Login.js
+++ components/Login.js
@@ -24,7 +24,7 @@ export default function Login() {
       {userInfo.checkingLogin ? (
         "Loading"
       ) : userInfo.loginStatus ? (
-        "You are logged in"
+        <div>You are logged in as {userInfo.userType}</div>
       ) : (
         <div className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
           <div className="w-full max-w-md space-y-8">
