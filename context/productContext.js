4f3b4fcbc785dff580b7e234b62b4e2041982b89
Imogen Minoli <imogen.minoli@gmail.com>
2023-06-07 14:02:30 +0100

    update

--- context/productContext.js
+++ context/productContext.js
@@ -144,7 +144,7 @@ export default function ProductProvider({ children }) {
         listingVariables,
         setListingVariables,
         getListingVariables,
-        resetProductInfo,
+        resetProductInfo, 
       }}
     >
       {children}
