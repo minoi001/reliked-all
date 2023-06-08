0fe0c57167e220cc0f9c51d75074be78bbb8c0e1
Alex Dresco <alex.dresco@birdie.care>
2023-06-08 10:37:43 +0200

    Collection filter without styling

--- pages/collections/[collection].js
+++ pages/collections/[collection].js
@@ -1,22 +1,31 @@
-import { getCollection } from "../../lib/shopify.js";
+import { Configure, Hits, RangeInput } from "react-instantsearch-dom";
+import ProductCard from "../../components/Products/ProductCard";
 
 export default function CollectionPage({ collection }) {
   // TODO: Some products seem to return empty (on Algolia but not on Shopify/ wrong handle?)
+  // 1. Import the RefinementList component
+  // 2. Query by default to the products with the handle = collection.handle
+  // 3. Create a few different filters: gender, price...
+  const searchParameters = {
+    query: "",
+    filters: `collections:"${collection}"`,
+    // Add any other search parameters here
+  };
   return (
     <div>
-      {collection.map((c, i) => (
-        <p key={i}>{c.node.title}</p>
-      ))}
+      <Configure {...searchParameters} />
+      <RangeInput attribute={"price"} />
+      <Hits hitComponent={ProductCard} />
     </div>
   );
 }
 export async function getServerSideProps({ params }) {
   // Fetch data based on the slug parameter
-  const collection = await getCollection(params.collection);
+  //const collection = await getCollection(params.collection);
 
   return {
     props: {
-      collection,
+      collection: params.collection,
     },
   };
 }
