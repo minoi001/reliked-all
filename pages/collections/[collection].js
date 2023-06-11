706a6cae1ffab59e171107e177e5dcca9762328c
Alex Dresco <alex.dresco@birdie.care>
2023-06-11 10:23:34 +0200

    Collections: shop by collection type

--- pages/collections/[collection].js
+++ pages/collections/[collection].js
@@ -2,14 +2,9 @@ import { Configure, Hits, RangeInput } from "react-instantsearch-dom";
 import ProductCard from "../../components/Products/ProductCard";
 
 export default function CollectionPage({ collection }) {
-  // TODO: Some products seem to return empty (on Algolia but not on Shopify/ wrong handle?)
-  // 1. Import the RefinementList component
-  // 2. Query by default to the products with the handle = collection.handle
-  // 3. Create a few different filters: gender, price...
   const searchParameters = {
     query: "",
     filters: `collections:"${collection}"`,
-    // Add any other search parameters here
   };
   return (
     <div>
--- pages/collections/index.js
+++ pages/collections/index.js
@@ -1,14 +1,21 @@
 import { getCollections } from "../../lib/shopify";
 import Link from "next/link";
+import { useState } from "react";
 
-export default function Collections({ collections }) {
+export default function Collections({ vendorsCollections }) {
+  // Filters: all, women, men, beauty, luxury ... metafields
+  const [collectionsDisplayed, setCollectionDisplayed] =
+    useState(vendorsCollections);
+  async function updateCollections(collectionType) {
+    const collectionsByType = await getCollections(collectionType);
+    setCollectionDisplayed(collectionsByType);
+  }
+
   return (
     <>
-      <h1 className="text-2xl font-extrabold text-gray-900 mb-6">
-        Shop by Influencer
-      </h1>
+      <Dropdown updateCollections={updateCollections} />
       <div className="grid grid-cols-2 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8 z-0">
-        {collections.map((collection, i) => {
+        {collectionsDisplayed.map((collection, i) => {
           return (
             <Link key={i} href={`collections/${collection.node.handle}`}>
               <img src={collection?.node?.image?.src} />
@@ -22,8 +29,75 @@ export default function Collections({ collections }) {
 }
 
 export async function getStaticProps() {
-  const collections = await getCollections();
+  const vendorsCollections = await getCollections("Vendor");
   return {
-    props: { collections },
+    props: { vendorsCollections },
   };
+}
+
+function Dropdown({ updateCollections }) {
+  return (
+    <>
+      <button
+        id="dropdownDefaultButton"
+        className="text-white bg-rose hover:bg-pink-800 font-medium rounded-lg text-sm px-4 py-2.5 text-center inline-flex items-center "
+        type="button"
+        onClick={() => {
+          const dropdown = document.getElementById("dropdown");
+          dropdown.classList.toggle("hidden");
+        }}
+      >
+        Shop By
+        <svg
+          className="w-4 h-4 ml-2"
+          aria-hidden="true"
+          fill="none"
+          stroke="currentColor"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M19 9l-7 7-7-7"
+          ></path>
+        </svg>
+      </button>
+      <div
+        id="dropdown"
+        className="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700"
+      >
+        <ul
+          className="py-2 text-sm text-gray-700 dark:text-gray-200"
+          aria-labelledby="dropdownDefaultButton"
+        >
+          <li className="hover:bg-gray-600">
+            <button
+              className=" px-4 py-2 text-left w-full"
+              onClick={() => updateCollections("Vendor")}
+            >
+              Influencer
+            </button>
+          </li>
+          <li className="hover:bg-gray-600">
+            <button
+              className=" px-4 py-2 text-left w-full"
+              onClick={() => updateCollections("Brand")}
+            >
+              Brand
+            </button>
+          </li>
+          <li className="hover:bg-gray-600">
+            <button
+              className=" px-4 py-2 text-left w-full"
+              onClick={() => updateCollections("Size")}
+            >
+              Size
+            </button>
+          </li>
+        </ul>
+      </div>
+    </>
+  );
 }
