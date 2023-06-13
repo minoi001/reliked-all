81f2097f9806995c0804c2f098831c1bc8b9ec4a
Imogen Minoli <imogen.minoli@gmail.com>
2023-06-13 17:00:15 +0100

    trying to get links to work for subheader categories without subcategories

--- components/Header/ShoppingSubheader.js
+++ components/Header/ShoppingSubheader.js
@@ -227,70 +227,52 @@ export default function ShoppingSubheader(props) {
                             className="space-y-10 px-4 pb-8 pt-10"
                           >
                             <div className="grid grid-cols-2 gap-x-4">
-                              {/* {category.featured.map((item) => (
-                            <div
-                              key={item.name}
-                              className="group relative text-sm"
-                            >
-                              <div className="aspect-h-1 aspect-w-1 overflow-hidden rounded-lg bg-gray-100 group-hover:opacity-75">
-                                <img
-                                  src={item.imageSrc}
-                                  alt={item.imageAlt}
-                                  className="object-cover object-center"
-                                />
-                              </div>
-                              <a
-                                href={item.href}
-                                className="mt-6 block font-medium text-gray-900"
-                              >
-                                <span
-                                  className="absolute inset-0 "
-                                  aria-hidden="true"
-                                />
-                                {item.name}
-                              </a>
-                              <p aria-hidden="true" className="mt-1">
-                                Shop now
-                              </p>
-                            </div>
-                          ))} */}
-                            </div>
-                            {category.items.map((section) => (
-                              <div key={section.name}>
-                                <a
-                                  href={section.href.replace(
-                                    "https://e-bloggers.myshopify.com",
-                                    ""
-                                  )}
-                                >
-                                  <p
-                                    id={`-heading-mobile`}
-                                    className="font-medium text-gray-900"
-                                  >
-                                    {section.name}
-                                  </p>
-                                </a>
-                                <ul
-                                  role="list"
-                                  aria-labelledby={`-heading-mobile`}
-                                  className="mt-6 flex flex-col space-y-6"
-                                >
-                                  {section.items.map((item) => (
-                                    <li key={item.name} className="flow-root">
+                              {category.items.length > 0 ? (
+                                <div>
+                                  {category.items.map((section) => (
+                                    <div key={section.name}>
                                       <a
-                                        href={item.href.replace(
+                                        href={section.href.replace(
                                           "https://e-bloggers.myshopify.com",
                                           ""
                                         )}
-                                        className="-m-2 block p-2 text-gray-500"
                                       >
-                                        {item.name}
+                                        <p
+                                          id={`-heading-mobile`}
+                                          className="font-medium text-gray-900"
+                                        >
+                                          {section.name}
+                                        </p>
                                       </a>
-                                    </li>
+                                      <ul
+                                        role="list"
+                                        aria-labelledby={`-heading-mobile`}
+                                        className="mt-6 flex flex-col space-y-6"
+                                      >
+                                        {section.items.map((item) => (
+                                          <li
+                                            key={item.name}
+                                            className="flow-root"
+                                          >
+                                            <a
+                                              href={item.href.replace(
+                                                "https://e-bloggers.myshopify.com",
+                                                ""
+                                              )}
+                                              className="-m-2 block p-2 text-gray-500"
+                                            >
+                                              {item.name}
+                                            </a>
+                                          </li>
+                                        ))}
+                                      </ul>
+                                    </div>
                                   ))}
-                                </ul>
-                              </div>
-                            ))}
+                                </div>
+                              ) : (
+                                <div></div>
+                              )}
+                            </div>
                           </Tab.Panel>
                         ))}
                       </Tab.Panels>
@@ -362,38 +344,58 @@ export default function ShoppingSubheader(props) {
                           {({ open }) => (
                             <>
                               <div className="relative flex">
-                                <Popover.Button
-                                  className={classNames(
-                                    open
-                                      ? "border-taupe text-taupe"
-                                      : "border-transparent text-gray-700 hover:text-gray-800",
-                                    "uppercase relative -mb-px flex items-center border-b-2 border-taupe pt-px text-sm font-medium transition-colors duration-200 ease-out"
-                                  )}
-                                >
-                                  {category.name}
-                                </Popover.Button>
+                                {category.items.length > 0 ? (
+                                  <Popover.Button
+                                    className={classNames(
+                                      open
+                                        ? "border-taupe text-taupe"
+                                        : "border-transparent text-gray-700 hover:text-gray-800",
+                                      "uppercase relative -mb-px flex items-center border-b-2 border-taupe pt-px text-sm font-medium transition-colors duration-200 ease-out"
+                                    )}
+                                  >
+                                    {category.name}
+                                  </Popover.Button>
+                                ) : (
+                                  <Link
+                                    href={category.href.replace(
+                                      "https://e-bloggers.myshopify.com",
+                                      ""
+                                    )}
+                                  >
+                                    <Popover.Button
+                                      className={classNames(
+                                        open
+                                          ? "border-taupe text-taupe"
+                                          : "border-transparent text-gray-700 hover:text-gray-800",
+                                        "uppercase relative -mb-px flex items-center border-b-2 border-taupe pt-px text-sm font-medium transition-colors duration-200 ease-out"
+                                      )}
+                                    >
+                                      {category.name}
+                                    </Popover.Button>
+                                  </Link>
+                                )}
                               </div>
-
-                              <Transition
-                                as={Fragment}
-                                enter="transition ease-out duration-200"
-                                enterFrom="opacity-0"
-                                enterTo="opacity-100"
-                                leave="transition ease-in duration-150"
-                                leaveFrom="opacity-100"
-                                leaveTo="opacity-0"
-                              >
-                                <Popover.Panel className="absolute inset-x-0 top-full text-sm text-gray-500">
-                                  {/* Presentational element used to render the bottom shadow, if we put the shadow on the actual panel it pokes out the top, so we use this shorter element to hide the top of the shadow */}
-                                  <div
-                                    className="absolute inset-0 top-1/2 bg-white shadow"
-                                    aria-hidden="true"
-                                  />
-                                  <div className="relative bg-white">
-                                    <div className="mx-auto max-w-7xl px-8">
-                                      <div className="grid grid-cols-2 gap-x-8 gap-y-10 py-16">
-                                        <div className="col-start-2 grid grid-cols-2 gap-x-8">
-                                          {/* {category.featured.map((item) => (
+                              {category.items.length > 0 ? (
+                                <Transition
+                                  as={Fragment}
+                                  enter="transition ease-out duration-200"
+                                  enterFrom="opacity-0"
+                                  enterTo="opacity-100"
+                                  leave="transition ease-in duration-150"
+                                  leaveFrom="opacity-100"
+                                  leaveTo="opacity-0"
+                                >
+                                  <Popover.Panel className="absolute inset-x-0 top-full text-sm text-gray-500">
+                                    {/* Presentational element used to render the bottom shadow, if we put the shadow on the actual panel it pokes out the top, so we use this shorter element to hide the top of the shadow */}
+                                    <div
+                                      className="absolute inset-0 top-1/2 bg-white shadow"
+                                      aria-hidden="true"
+                                    />
+                                    <div className="relative bg-white">
+                                      <div className="mx-auto max-w-7xl px-8">
+                                        <div className="grid grid-cols-2 gap-x-8 gap-y-10 py-16">
+                                          <div className="col-start-2 grid grid-cols-2 gap-x-8">
+                                            {/* {category.featured.map((item) => (
                                             <div
                                               key={item.name}
                                               className="group relative text-base sm:text-sm"
@@ -423,39 +425,42 @@ export default function ShoppingSubheader(props) {
                                               </p>
                                             </div>
                                           ))} */}
-                                        </div>
-                                        <div className="row-start-1 grid grid-cols-3 gap-x-2 gap-y-6 text-sm">
-                                          {category.items.map((section) => (
-                                            <div
-                                              key={section.name}
-                                              className="inline"
-                                            >
-                                              {/* NEEDS TO BE HOVER MENUS */}
-                                              <Link
-                                                href={section.href.replace(
-                                                  "https://e-bloggers.myshopify.com",
-                                                  ""
-                                                )}
+                                          </div>
+                                          <div className="row-start-1 grid grid-cols-3 gap-x-2 gap-y-6 text-sm">
+                                            {category.items.map((section) => (
+                                              <div
+                                                key={section.name}
+                                                className="inline"
                                               >
-                                                <div key={section.name}>
-                                                  {MiniDropdownMenu(
-                                                    section.name,
-                                                    section.href.replace(
-                                                      "https://e-bloggers.myshopify.com",
-                                                      ""
-                                                    ),
-                                                    section.items
+                                                {/* NEEDS TO BE HOVER MENUS */}
+                                                <Link
+                                                  href={section.href.replace(
+                                                    "https://e-bloggers.myshopify.com",
+                                                    ""
                                                   )}
-                                                </div>
-                                              </Link>
-                                            </div>
-                                          ))}
+                                                >
+                                                  <div key={section.name}>
+                                                    {MiniDropdownMenu(
+                                                      section.name,
+                                                      section.href.replace(
+                                                        "https://e-bloggers.myshopify.com",
+                                                        ""
+                                                      ),
+                                                      section.items
+                                                    )}
+                                                  </div>
+                                                </Link>
+                                              </div>
+                                            ))}
+                                          </div>
                                         </div>
                                       </div>
                                     </div>
-                                  </div>
-                                </Popover.Panel>
-                              </Transition>
+                                  </Popover.Panel>
+                                </Transition>
+                              ) : (
+                                ""
+                              )}
                             </>
                           )}
                         </Popover>
