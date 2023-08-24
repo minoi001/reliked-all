ae611f59e5ce7146a39961078c3b29d0fd2b8fc0
Imogen <90554214+minoi001@users.noreply.github.com>
2023-08-24 16:51:29 +0100

    cannot get drop downs on main menu to show on top

--- components/Header/MiniDropdownMenu.js
+++ components/Header/MiniDropdownMenu.js
@@ -1,33 +1,61 @@
 import { Menu, Transition } from "@headlessui/react";
 import { ChevronDownIcon } from "@heroicons/react/24/outline";
 import Link from "next/link";
+function classNames(...classes) {
+  return classes.filter(Boolean).join(" ");
+}
 
 export default function MiniDropdownMenu(menuTitle, menuHandle, menuItems) {
   return (
-    <div>
-      <div class="peer px-5 py-2 flex">
-        <Link href={menuHandle}>{menuTitle}</Link>
+    <Menu as="div" className="relative inline-block text-left">
+      <div className="flex">
         {menuItems.length > 0 ? (
-          <ChevronDownIcon className="inline-flex w-5 h-5 p-1" />
+          <div>
+            <Menu.Button className="flex">
+              {menuTitle}
+              <ChevronDownIcon className="flex w-5 h-5 p-1" />
+            </Menu.Button>
+            <div className="relative z-100">
+              <Transition
+                enter="transition ease-out duration-100"
+                enterFrom="transform opacity-0 scale-95"
+                enterTo="transform opacity-100 scale-100"
+                leave="transition ease-in duration-75"
+                leaveFrom="transform opacity-100 scale-100"
+                leaveTo="transform opacity-0 scale-95"
+              >
+                <Menu.Items className="absolute left-0 mt-2 w-32 origin-top-left bg-offWhite shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                  <div className="py-1">
+                    {menuItems.map((item) => (
+                      <div key={item.name}>
+                        <Menu.Item>
+                          {({ active }) => (
+                            <Link
+                              href={item.href}
+                              className={classNames(
+                                active
+                                  ? "bg-cream text-black"
+                                  : "text-almostBlack",
+                                "block px-4 py-2 text-sm"
+                              )}
+                            >
+                              {item.name}
+                            </Link>
+                          )}
+                        </Menu.Item>
+                      </div>
+                    ))}
+                  </div>
+                </Menu.Items>
+              </Transition>
+            </div>
+          </div>
         ) : (
-          <div></div>
+          <Link href={menuHandle}>
+            <button className="flex">{menuTitle}</button>
+          </Link>
         )}
       </div>
-      <div
-        class="hidden peer-hover:flex hover:flex
-        w-[200px]
-        flex-col bg-white drop-shadow-lg absolute"
-      >
-        {menuItems.map((item) => (
-          <Link
-            href={item.href}
-            key={item.name}
-            class="px-5 py-3 hover:bg-gray-200"
-          >
-            {item.name}
-          </Link>
-        ))}
-      </div>
-    </div>
+    </Menu>
   );
 }
