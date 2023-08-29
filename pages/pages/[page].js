702fd2e95585617757940ef1d83fb15fdd54ca53
Imogen <90554214+minoi001@users.noreply.github.com>
2023-08-29 13:20:33 +0100

    blog and page templates

---
+++ pages/pages/[page].js
@@ -0,0 +1,27 @@
+import ProductPageContent from "../../components/Product/ProductPageContent";
+import { getProduct } from "../../lib/shopify.js";
+import Head from "next/head";
+
+export default function ProductPage({ product, id }) {
+  return (
+    <div className="minh-screen">
+      <Head>
+        <title>{product.title}</title>
+      </Head>
+      <title>{product.title}</title>
+      <meta name="description" content={product.description} />;
+      <ProductPageContent product={product} id={id} />
+    </div>
+  );
+}
+export async function getServerSideProps({ params, query }) {
+  // Fetch data based on the slug parameter
+  const product = await getProduct(params.product);
+
+  return {
+    props: {
+      product,
+      id: query.id,
+    },
+  };
+}
