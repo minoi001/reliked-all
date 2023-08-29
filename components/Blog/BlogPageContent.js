b22b2b0cef699c8829cdb173260410ee8765b6a5
Imogen <90554214+minoi001@users.noreply.github.com>
2023-08-29 17:19:03 +0100

    add space

--- components/Blog/BlogPageContent.js
+++ components/Blog/BlogPageContent.js
@@ -5,7 +5,7 @@ export default function BlogPageContent({ blog, id }) {
     <div className="mx-auto max-w-7xl px-4 md:px-6 lg:px-8 mt-2 md:mt-6">
       <div className="md:px-12 align-middle p-2 py-2 w-full bg-white shadow-lg">
         <div className="w-full p-4 py-12  text-center">
-          <h1 className="font-h text-3xl">{blog.title}Blog</h1>
+          <h1 className="font-h text-3xl">{blog.title} Blog</h1>
           <p>hi</p>
           {/* <div
             className="pt-6 pb-4 items-center"
