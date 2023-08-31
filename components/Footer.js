0fe0a2bea9e4546be4ad97251ff1327e2fe2c76f
Imogen <90554214+minoi001@users.noreply.github.com>
2023-08-31 16:46:51 +0100

    social icons

--- components/Footer.js
+++ components/Footer.js
@@ -3,7 +3,12 @@ import Link from "next/link";
 import { useContext } from "react";
 import { ShopContext } from "../context/shopContext";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faFacebook, faInstagram } from "@fortawesome/free-brands-svg-icons";
+import {
+  faFacebook,
+  faInstagram,
+  faTiktok,
+  faThreads,
+} from "@fortawesome/free-brands-svg-icons";
 
 export default function Footer(props) {
   const { footerNav } = useContext(ShopContext);
@@ -27,66 +32,58 @@ export default function Footer(props) {
             All rights reserved
           </p>
           <div className="flex items-center gap-x-4 mt-12">
-            <button
-              aria-label="instagram"
-              className="focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-800 bg-opacity-20 w-8 h-8 flex-shrink-0 bg-white cursor-pointer hover:bg-almostBlack hover:bg-opacity-20 rounded-full flex items-center justify-center"
-            >
-              <FontAwesomeIcon
-                icon={faInstagram}
-                color="white"
-                width="20"
-                height="20"
-              />
-            </button>
-            <button
-              aria-label="facebook"
-              className="focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-800 bg-opacity-20 w-8 h-8 flex-shrink-0 bg-white cursor-pointer hover:bg-almostBlack hover:bg-opacity-20 rounded-full flex items-center justify-center"
-            >
-              <FontAwesomeIcon
-                icon={faFacebook}
-                color="white"
-                width="20"
-                height="20"
-              />
-            </button>
-            <button
-              aria-label="twitter"
-              className="focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-800 opacity-50 w-8 h-8 flex-shrink-0 bg-gray-800 cursor-pointer hover:bg-gray-700 rounded-full flex items-center justify-center"
-            >
-              <svg
-                width="16"
-                height="13"
-                viewBox="0 0 16 13"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
+            <a target="_blank" href="https://instagram.com/reliked">
+              <button
+                aria-label="instagram"
+                className="focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-800 bg-opacity-20 w-8 h-8 flex-shrink-0 bg-white cursor-pointer hover:bg-almostBlack hover:bg-opacity-20 rounded-full flex items-center justify-center"
               >
-                <path
-                  fillRule="evenodd"
-                  clipRule="evenodd"
-                  d="M7.5208 3.59864L7.55438 4.13498L6.99479 4.0693C4.95791 3.81755 3.17843 2.9638 1.66756 1.52992L0.928908 0.818458L0.73865 1.34385C0.33575 2.51503 0.593158 3.75188 1.43253 4.58375C1.8802 5.04346 1.77948 5.10914 1.00725 4.8355C0.73865 4.74793 0.503625 4.68226 0.481242 4.71509C0.4029 4.79171 0.6715 5.78776 0.884142 6.18181C1.17513 6.72909 1.76828 7.26542 2.4174 7.58284L2.96579 7.83459L2.31668 7.84554C1.68994 7.84554 1.66756 7.85648 1.73471 8.08634C1.95854 8.79781 2.84268 9.55305 3.82755 9.88142L4.52143 10.1113L3.91708 10.4615C3.02175 10.965 1.96973 11.2496 0.917717 11.2715C0.414092 11.2825 0 11.3262 0 11.3591C0 11.4685 1.36538 12.0815 2.15999 12.3223C4.54382 13.0338 7.37531 12.7273 9.50173 11.5123C11.0126 10.6476 12.5235 8.92915 13.2286 7.26542C13.6091 6.37883 13.9896 4.75888 13.9896 3.98174C13.9896 3.47824 14.0232 3.41257 14.6499 2.81056C15.0192 2.4603 15.3662 2.0772 15.4333 1.96775C15.5452 1.75978 15.534 1.75978 14.9633 1.94586C14.012 2.27422 13.8777 2.23044 14.3477 1.73789C14.6947 1.38763 15.1088 0.752784 15.1088 0.566709C15.1088 0.533872 14.9409 0.5886 14.7506 0.68711C14.5492 0.796566 14.1015 0.96075 13.7658 1.05926L13.1614 1.24534L12.613 0.884131C12.3108 0.68711 11.8856 0.468198 11.6617 0.402524C11.0909 0.249286 10.218 0.271177 9.70318 0.446307C8.30422 0.938859 7.42008 2.20855 7.5208 3.59864Z"
-                  fill="white"
+                <FontAwesomeIcon
+                  icon={faInstagram}
+                  color="white"
+                  width="20"
+                  height="20"
                 />
-              </svg>
-            </button>
-            <button
-              aria-label="youtube"
-              className="focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-800 opacity-50 w-8 h-8 flex-shrink-0 bg-gray-800 cursor-pointer hover:bg-gray-700 rounded-full flex items-center justify-center"
-            >
-              <svg
-                width="18"
-                height="13"
-                viewBox="0 0 18 13"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
+              </button>
+            </a>
+            <a target="_blank" href="https://www.threads.net/@reliked">
+              <button
+                aria-label="threads"
+                className="focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-800 bg-opacity-20 w-8 h-8 flex-shrink-0 bg-white cursor-pointer hover:bg-almostBlack hover:bg-opacity-20 rounded-full flex items-center justify-center"
               >
-                <path
-                  fillRule="evenodd"
-                  clipRule="evenodd"
-                  d="M15.6677 1.17143C16.4021 1.36664 16.9804 1.94183 17.1767 2.67227C17.5333 3.99611 17.5333 6.75832 17.5333 6.75832C17.5333 6.75832 17.5333 9.52043 17.1767 10.8444C16.9804 11.5748 16.4021 12.15 15.6677 12.3453C14.3369 12.7 9.00001 12.7 9.00001 12.7C9.00001 12.7 3.66309 12.7 2.33218 12.3453C1.59783 12.15 1.0195 11.5748 0.823232 10.8444C0.466675 9.52043 0.466675 6.75832 0.466675 6.75832C0.466675 6.75832 0.466675 3.99611 0.823232 2.67227C1.0195 1.94183 1.59783 1.36664 2.33218 1.17143C3.66309 0.81665 9.00001 0.81665 9.00001 0.81665C9.00001 0.81665 14.3369 0.81665 15.6677 1.17143ZM7.40002 4.43326V9.59993L11.6667 7.01669L7.40002 4.43326Z"
-                  fill="white"
+                <FontAwesomeIcon
+                  icon={faThreads}
+                  color="white"
+                  width="20"
+                  height="20"
                 />
-              </svg>
-            </button>
+              </button>{" "}
+            </a>
+            <a target="_blank" href="https://www.facebook.com/RelikedUK/">
+              <button
+                aria-label="facebook"
+                className="focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-800 bg-opacity-20 w-8 h-8 flex-shrink-0 bg-white cursor-pointer hover:bg-almostBlack hover:bg-opacity-20 rounded-full flex items-center justify-center"
+              >
+                <FontAwesomeIcon
+                  icon={faFacebook}
+                  color="white"
+                  width="20"
+                  height="20"
+                />
+              </button>{" "}
+            </a>
+            <a target="_blank" href="https://www.tiktok.com/@reliked">
+              <button
+                aria-label="tiktok"
+                className="focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-800 bg-opacity-20 w-8 h-8 flex-shrink-0 bg-white cursor-pointer hover:bg-almostBlack hover:bg-opacity-20 rounded-full flex items-center justify-center"
+              >
+                <FontAwesomeIcon
+                  icon={faTiktok}
+                  color="white"
+                  width="20"
+                  height="20"
+                />
+              </button>
+            </a>
           </div>
         </div>
         <div className="sm:ml-0 ml-1 w-full grid grid-cols-2">
