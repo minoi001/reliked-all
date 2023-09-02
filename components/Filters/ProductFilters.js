f787907761882ce9e0fd19741abd2b0e1e50311b
Alex Dresco <alexandra.d1510@gmail.com>
2023-09-02 13:49:11 +0100

    algolia dropdown logic added to sortby

--- components/Filters/ProductFilters.js
+++ components/Filters/ProductFilters.js
@@ -1,21 +1,36 @@
 import { SortBy } from "react-instantsearch";
 import ProductFiltersWithoutSortBy from "./ProductFiltersWithoutSortBy";
 import CustomSortBy from "./CustomSortBy";
+import { useState } from "react";
+import AlgoliaDropdownMenu from "../AlgoliaDropdownMenu";
 
 const ProductFilters = ({ toggleSlideover, isFromSearch, refine }) => {
+  const [isSortByOpen, setIsSortByOpen] = useState();
   return (
     // Also need to add a fixed height to the dropdown menus and add a scroll, preferably with an apply button fixed at the bottom of the dropdown menu which closes it
     // styling on algolia dropdown menu needs to be full width of the refinements/range inputs
     <>
       <div className="max-h-min lg:flex md:pb-4 sm:static sm:inline">
-        <CustomSortBy
-          className="mb-4 md:mb-0"
-          items={[
-            { value: "shopify_products_published_at_desc", label: "Newest in" },
-            { value: "shopify_products_price_desc", label: "Price descending" },
-            { value: "shopify_products_price_asc", label: "Price ascending" },
-          ]}
-        />
+        {AlgoliaDropdownMenu(
+          "Sort By",
+          <CustomSortBy
+            className="mb-4 md:mb-0"
+            items={[
+              {
+                value: "shopify_products_published_at_desc",
+                label: "Newest in",
+              },
+              {
+                value: "shopify_products_price_desc",
+                label: "Price descending",
+              },
+              { value: "shopify_products_price_asc", label: "Price ascending" },
+            ]}
+          />,
+          isSortByOpen,
+          () => setIsSortByOpen(!isSortByOpen),
+          setIsSortByOpen
+        )}
       </div>
       <div className="hidden lg:block">
         <ProductFiltersWithoutSortBy format={"row"} />
