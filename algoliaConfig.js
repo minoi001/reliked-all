278a78fcf7275fb48d91dda805aab7d87ccb3618
Alex Dresco <alex.dresco@birdie.care>
2023-09-12 21:10:21 +0200

    Get product ID from Algolia

--- algoliaConfig.js
+++ algoliaConfig.js
@@ -10,4 +10,21 @@ const indexNames = {
   collections: "shopify_collections",
 };
 
-export { searchClient, indexNames };
+const index = searchClient.initIndex(indexNames.products);
+async function getObjectIDByProductHandle(productHandle) {
+  try {
+    const { hits } = await index.search(productHandle);
+    if (hits.length > 0) {
+      // Assuming there's only one matching result, you can access its objectID.
+      const objectID = hits[0].objectID;
+      return `${objectID}`;
+    } else {
+      // Handle the case when no results are found for the given handle.
+      return "";
+    }
+  } catch (error) {
+    console.error("***Error fetching Algolia data:", error);
+    return null;
+  }
+}
+export { searchClient, indexNames, getObjectIDByProductHandle };
