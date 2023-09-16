075aa58bde7300911ce0c9680df25dd210b0c7e4
Alex Dresco <alex.dresco@birdie.care>
2023-09-16 16:43:12 +0200

    Fix the slide out component so that the labels don't come on top

--- components/SlideOut.js
+++ components/SlideOut.js
@@ -7,7 +7,7 @@ export default function SlideOut({ isSlideOverOpen, toggleSlideover }) {
       id="slideover-container"
       className={
         isSlideOverOpen
-          ? "w-full h-full fixed inset-0"
+          ? "w-full h-full fixed inset-0 z-50"
           : "w-full h-full fixed inset-0 invisible"
       }
     >
@@ -16,14 +16,14 @@ export default function SlideOut({ isSlideOverOpen, toggleSlideover }) {
         id="slideover-bg"
         className={
           isSlideOverOpen
-            ? "w-full h-full  duration-300 transition ease-in-out transition-all inset-0 absolute bg-gray-900 opacity-50"
-            : "w-full h-full  duration-300 transition ease-in-out transition-all inset-0 absolute bg-gray-900 opacity-0"
+            ? "w-full h-full duration-300 transition ease-in-out transition-all inset-0 absolute bg-gray-900 opacity-50 z-40"
+            : "w-full h-full duration-300 transition ease-in-out transition-all inset-0 absolute bg-gray-900 opacity-0"
         }
       ></div>
       <div
         id="slideover"
         className={
-          "w-2/3 bg-white h-full absolute right-0 duration-300 transition ease-in-out transition-all"
+          "w-2/3 bg-white h-full absolute right-0 duration-300 transition ease-in-out transition-all z-50"
         }
       >
         <div className="absolute cursor-pointer text-gray-600 duration-300 transition ease-in-out top-0 w-8 h-8 flex items-center justify-center right-0 mt-2 mr-2">
