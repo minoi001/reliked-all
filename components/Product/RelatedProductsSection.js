d024f07647577b67684d91ed3a785fbb5b736b8f
Alex Dresco <alex.dresco@birdie.care>
2023-09-24 19:26:34 +0200

    Start of scrollable you may also like

--- components/Product/RelatedProductsSection.js
+++ components/Product/RelatedProductsSection.js
@@ -1,8 +1,5 @@
-import React from "react";
-import {
-  FrequentlyBoughtTogether,
-  RelatedProducts,
-} from "@algolia/recommend-react";
+import React, { useState } from "react";
+import { useRelatedProducts } from "@algolia/recommend-react";
 import recommend from "@algolia/recommend";
 import ProductCard from "../Products/ProductCard";
 
@@ -13,6 +10,11 @@ const recommendClient = recommend(
 const indexName = "shopify_products";
 
 export default function RelatedProductsSection({ currentObjectID }) {
+  const { recommendations } = useRelatedProducts({
+    recommendClient,
+    indexName,
+    objectIDs: [currentObjectID],
+  });
   return (
     <div>
       {/*TODO: We don't yet have the conversion events to enable this*/}
@@ -29,25 +31,49 @@ export default function RelatedProductsSection({ currentObjectID }) {
       {/*  }}*/}
       {/*/>*/}
 
-      <RelatedProducts
-        recommendClient={recommendClient}
-        indexName={indexName}
-        objectIDs={[currentObjectID]}
-        itemComponent={({ item }) => {
-          return <ProductCard hit={item} />;
-        }}
-        maxRecommendations={12}
-        headerComponent={() => {
-          return (
-            <h2 className="font-h mx-auto my-4 max-w-7xl px-4 sm:px-6 lg:px-8 text-3xl font-extrabold text-gray-900 mt-6">
-              You may also like...
-            </h2>
-          );
-        }}
-        classNames={{
-          list: "grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 xl:gap-8",
-        }}
-      />
+      <h2 className="font-h mx-auto my-4 max-w-7xl px-4 sm:px-6 lg:px-8 text-3xl font-extrabold text-gray-900 mt-6">
+        You may also like...
+      </h2>
+
+      <ProductCarousel recommendations={recommendations} />
     </div>
   );
 }
+
+const ProductCarousel = ({ recommendations }) => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const handleNextClick = () => {
+    setCurrentIndex(currentIndex + 1);
+  };
+  const handlePrevClick = () => {
+    setCurrentIndex(currentIndex - 1);
+  };
+
+  return (
+    <div className="flex">
+      {currentIndex > 0 && (
+        <div className="" onClick={handlePrevClick}>
+          &lt;
+        </div>
+      )}
+
+      <div className="grid grid-cols-4 lg:grid-cols-4 xl:grid-cols-4 gap-6 xl:gap-4">
+        {recommendations
+          .filter((_, index) => {
+            return index >= currentIndex && index < 4 + currentIndex;
+          })
+          .map((recommendation, index) => (
+            <div key={index}>
+              <ProductCard hit={recommendation} key={recommendation.objectID} />
+            </div>
+          ))}
+      </div>
+      {currentIndex < 2 && (
+        <div className="" onClick={handleNextClick}>
+          &gt;
+        </div>
+      )}
+    </div>
+  );
+};
