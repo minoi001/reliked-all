099220baa1d5d0f4df6fb9c6a910fb26f5e0048f
Imogen <90554214+minoi001@users.noreply.github.com>
2023-10-12 11:33:45 +0100

    image sizes

--- components/Products/ProductCard.js
+++ components/Products/ProductCard.js
@@ -41,7 +41,7 @@ const ProductCard = ({ hit }) => {
           <div className="group w-full bg-offWhite overflow-hidden ">
             <div className="grid group-hover:opacity-75-20 w-full aspect-4/5 p-3 place-items-center">
               <Image
-                sizes="(max-width: 768px) 45vw, (max-width: 1200px) 30vw, 20vw"
+                sizes="(max-width: 768px) 30vw, (max-width: 1200px) 20vw, 10vw"
                 src={image}
                 alt={altText}
                 width="446"
