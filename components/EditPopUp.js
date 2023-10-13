6b430c64af5ab386c660656f7ee633be46b54d23
Imogen <90554214+minoi001@users.noreply.github.com>
2023-10-13 15:28:48 +0100

    remove round edged

--- components/EditPopUp.js
+++ components/EditPopUp.js
@@ -85,7 +85,7 @@ export default function PopUp({ open, setOpen, content }) {
               leaveFrom="opacity-100 translate-y-0 sm:scale-100"
               leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
             >
-              <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+              <Dialog.Panel className="relative transform overflow-hidden bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
                 <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
                   <div className="sm:flex sm:items-start">
                     <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-cream sm:mx-0 sm:h-10 sm:w-10">
@@ -151,13 +151,13 @@ export default function PopUp({ open, setOpen, content }) {
                           <div className="bg-white px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
                             <button
                               type="submit"
-                              className="inline-flex w-full justify-center rounded-md bg-mint px-3 py-2 text-sm font-semibold text-almostBlack shadow-sm hover:bg-almostBlack hover:text-white sm:ml-3 sm:w-auto"
+                              className="inline-flex w-full justify-center bg-mint px-3 py-2 text-sm font-semibold text-almostBlack shadow-sm hover:bg-almostBlack hover:text-white sm:ml-3 sm:w-auto"
                             >
                               Update
                             </button>
                             <button
                               type="button"
-                              className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
+                              className="mt-3 inline-flex w-full justify-center bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
                               onClick={() => setOpen(false)}
                               ref={cancelButtonRef}
                             >
