738cbbd7ae66ba20c7ffb7b45e4851b157062e9c
Alex Dresco <alex.dresco@birdie.care>
2024-01-31 20:44:05 +0100

    search resets scroll position

--- components/Header/SearchBox.js
+++ components/Header/SearchBox.js
@@ -1,17 +1,21 @@
-import React from "react";
+import React, { useContext } from "react";
 import { SearchBox } from "react-instantsearch";
 import { useRouter } from "next/navigation";
 // Include only the reset
 import "instantsearch.css/themes/reset.css";
 import { event } from "../../lib/ga";
+import { ProductContext } from "../../context/productContext";
 
 export const Search = () => {
   const router = useRouter();
+  const { setScrollPosition } = useContext(ProductContext);
+
   const onSubmit = (e) => {
     e.preventDefault();
     const searchTerm = e.currentTarget?.elements[0]?.value;
     event("search", { search_term: searchTerm });
     router.push(`/search?q=${searchTerm.trim().replace(" ", "+")}`);
+    setScrollPosition(0);
   };
 
   const onReset = () => {
